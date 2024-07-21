0091e34f0d375ace8854e3a53c5d1d5075a7fd82
rukirukiruk <163348244+rukirukiruk@users.noreply.github.com>
2024-07-21 11:53:09 +0200

    add error catching

--- routes.js
+++ routes.js
@@ -3,14 +3,49 @@ const habitController = require('./habitController');
 
 const router = express.Router();
 
-router.post('/habits', habitController.createHabit);
+router.post('/habits', async (req, res, next) => {
+    try {
+        await habitController.createHabit(req, res);
+    } catch (error) {
+        next(error);
+    }
+});
 
-router.get('/habits', habitController.getAllHabits);
+router.get('/habits', async (req, res, next) => {
+    try {
+        await habitController.getAllHabits(req, res);
+    } catch (error) {
+        next(error);
+    }
+});
 
-router.get('/habits/:id', habitController.getHabitById);
+router.get('/habits/:id', async (req, res, next) => {
+    try {
+        await habitController.getHabitById(req, res);
+    } catch (error) {
+        next(error);
+    }
+});
 
-router.put('/habits/:id', habitController.updateHabit);
+router.put('/habits/:id', async (req, res, next) => {
+    try {
+        await habitController.updateHabit(req, res);
+    } catch (error) {
+        next(error);
+    }
+});
 
-router.delete('/habits/:id', habitController.deleteHabit);
+router.delete('/habits/:id', async (req, res, next) => {
+    try {
+        await habitController.deleteHabit(req, res);
+    } catch (error) {
+        next(error);
+    }
+});
 
-module.exports = a router;
+router.use((error, req, res, next) => {
+    console.error(error.stack);
+    res.status(500).send('Something broke!');
+});
+
+module.exports = router;
