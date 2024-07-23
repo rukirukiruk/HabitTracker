e20987ec0cbaf19fffb9c41582cff9d387f0bafa
rukirukiruk <163348244+rukirukiruk@users.noreply.github.com>
2024-07-23 10:54:55 +0200

    added new feature

--- routes.js
+++ routes.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const habitController = require('./habitController');
+const { celebrate, Joi, errors } = require('celebrate'); // Add Joi validation
 
 const router = express.Router();
 
-router.post('/habits', async (req, res, next) => {
+// Validation schema
+const habitSchema = Joi.object({
+    name: Joi.string().required(),
+    description: Joi.string().allow(''),
+    streak: Joi.number().integer().min(0).optional()
+});
+
+router.post('/habits', celebrate({ body: habitSchema }), async (req, res, next) => {
     try {
         await habitController.createHabit(req, res);
     } catch (error) {
@@ -23,11 +31,11 @@ router.get('/habits/:id', async (req, res, next) => {
     try {
         await habitController.getHabitById(req, res);
     } catch (error) {
-        next(error);
+      next(error);
     }
 });
 
-router.put('/habits/:id', async (req, res, next) => {
+router.put('/habits/:id', celebrate({ body: habitSchema }), async (req, res, next) => {
     try {
         await habitController.updateHabit(req, res);
     } catch (error) {
@@ -42,6 +50,9 @@ router.delete('/habits/:id', async (req, res, next) => {
         next(error);
     }
 });
+
+// Error handler for validation errors
+router.use(errors());
 
 router.use((error, req, res, next) => {
     console.error(error.stack);
